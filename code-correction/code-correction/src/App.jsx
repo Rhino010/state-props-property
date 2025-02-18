082c56d296a94fa835a9d2fe57a96b33d197bcf1
Ryan Boll <bollr38@hotmail.com>
2025-02-18 15:38:28 -0600

    error fixes

---
+++ code-correction/code-correction/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import './App.css'
+
+
+class UserProfile extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { name: 'Alex' };
+  }
+
+  // Incorrect function
+  changeName() {
+      this.state.name = 'Charlie';
+  }
+
+  render() {
+      return (
+          <div>
+              <h1>User Profile</h1>
+              <p>Name: {this.state.name}</p>
+              <button onClick={this.changeName}>Change Name</button>
+          </div>
+      );
+  }
+}
+
+export default App;
+
+
